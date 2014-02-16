208420f4bae1b756800909f01581b1c4db4be092
hamdi <hamdi@hamdi-PC.esprit.tn>
2014-02-16 10:09:55 +0100

    njarbou,jkkn,^yp*,

---
+++ src/BestDeal/entities/Deals.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package BestDeal.entities;
+
+import java.util.Date;
+
+/**
+ *
+ * @author hamdi
+ */
+public class Deals {
+    int Id_Deal;
+    String Nom_Deal;
+    String Description_Deal;
+    String Description_Detaillé_Deal;
+    String Image;
+    Double Prix_De_Vente;
+    Double Prix_Original_Deal;
+    Date Date_Publication_Deal;
+    Date Date
+    
+    
+    
+    
+}
